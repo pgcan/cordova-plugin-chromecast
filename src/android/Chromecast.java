d1b8a56efbaf3efa312f0d57c1ff585dd8a58937
pgcan <prasoon.can@gmail.com>
2021-02-02 11:13:40 +0530

    import java.util.Collections;

--- src/android/Chromecast.java
+++ src/android/Chromecast.java
@@ -12,6 +12,7 @@ import android.text.Spanned;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
